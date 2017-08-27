90494584c8a38bebe7cea441e46dc4eb5d2eb886
cgewecke <christophergewecke@gmail.com>
2017-08-27 16:28:51 -0700

    Add rules for newer assembly features & assembly assignment fix

--- test/doc_examples.sol
+++ test/doc_examples.sol
@@ -442,3 +442,85 @@ contract tupleAssignmentToMemberExpression {
     //(,vote.voted[msg.sender]) = isYay ? (0,1) : (0,2);
   }
 }
+
+library Array256Lib {
+  function sumElements(uint256[] storage self) constant returns(uint256 sum) {
+    assembly {
+      mstore(0x60,self_slot)
+
+      for { let i := 0 } lt(i, sload(self_slot)) { i := add(i, 1) } {
+        sum := add(sload(add(sha3(0x60,0x20),i)),sum)
+      }
+    }
+  }
+
+  function getMax(uint256[] storage self) constant returns(uint256 maxValue) {
+    assembly {
+      mstore(0x60,self_slot)
+      maxValue := sload(sha3(0x60,0x20))
+
+      for { let i := 0 } lt(i, sload(self_slot)) { i := add(i, 1) } {
+        switch gt(sload(add(sha3(0x60,0x20),i)), maxValue)
+        case 1 {
+          maxValue := sload(add(sha3(0x60,0x20),i))
+        }
+      }
+    }
+  }
+
+  function getMin(uint256[] storage self) constant returns(uint256 minValue) {
+    assembly {
+      mstore(0x60,self_slot)
+      minValue := sload(sha3(0x60,0x20))
+
+      for { let i := 0 } lt(i, sload(self_slot)) { i := add(i, 1) } {
+        switch gt(sload(add(sha3(0x60,0x20),i)), minValue)
+        case 0 {
+          minValue := sload(add(sha3(0x60,0x20),i))
+        }
+      }
+    }
+  }
+
+  function indexOf(uint256[] storage self, uint256 value, bool isSorted) constant
+           returns(bool found, uint256 index) {
+    assembly{
+      mstore(0x60,self_slot)
+      switch isSorted
+      case 1 {
+        let high := sub(sload(self_slot),1)
+        let mid := 0
+        let low := 0
+        for { } iszero(gt(low, high)) { } {
+          mid := div(add(low,high),2)
+
+          switch lt(sload(add(sha3(0x60,0x20),mid)),value)
+          case 1 {
+             low := add(mid,1)
+          }
+          case 0 {
+            switch gt(sload(add(sha3(0x60,0x20),mid)),value)
+            case 1 {
+              high := sub(mid,1)
+            }
+            case 0 {
+              found := 1
+              index := mid
+              low := add(high,1)
+            }
+          }
+        }
+      }
+      case 0 {
+        for { let low := 0 } lt(low, sload(self_slot)) { low := add(low, 1) } {
+          switch eq(sload(add(sha3(0x60,0x20),low)), value)
+          case 1 {
+            found := 1
+            index := low
+            low := sload(self_slot)
+          }
+        }
+      }
+    }
+  }
+}
